afe14eeaf7665d5efe3a51d5b1ec62fbf5da890d
Scott Toney <ascotttoney@gmail.com>
2019-02-17 18:14:52 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,18 +1,35 @@
+# board is an array of strings
+# ["X", " ", " ", " ", "0", " "]
+# index is a number to check
+# it could be any number in the array
+
+# puts board.inspect
+# puts index
+# puts board[index].inspect
+
+# def position_taken?(board, index)
+#   if board[0] == " "
+#     return false
+#   end
+#
+#   if board[0] == ""
+#     return false
+#   end
+#
+#   if board[0] == nil
+#     return false
+#   end
+#
+#   if board[0] == "X" || "O"
+#     return true
+#   end
+# end
+
 def position_taken?(board, index)
-  if board[0] == " "
+  position = board[index]
+  if position == "" || position == " " || position == nil
     return false
-  end
-
-  if board[0] == ""
-    return false
-  end
-
-  if board[0] == nil
-    return false
-  end
-
-  if board[0] == "X" || "O"
+  else
     return true
   end
-
 end
